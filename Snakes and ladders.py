b7f6b49cd819016173f8b7bb271fa63f31641cc6
kmanchu <leafhockey13@gmail.com>
2018-06-10 15:30:21 -0400

    made log to track events

--- Snakes and ladders.py
+++ Snakes and ladders.py
@@ -1,4 +1,14 @@
-#2-4 players for this game
+import logging
+logging.basicConfig(filename='snakes_ladders.log',level=logging.DEBUG)
+"""
+-setup game requires
+-number of players
+-postion of players
+-Dice function
+
+
+"""
+logging.info('Start of program')
 def setup_game():
  players= 2
  while True:
@@ -6,12 +16,38 @@ def setup_game():
           print("how many players are in the game?")
           players= int(input())
           if players > 4 or players < 2:
-            print("must be lower than 5 or higher than 1")
+              print("must be lower than 5 or higher than 1")
           else:
-            break
+              #the right number of players had been defined...
+              logging.info("number of players created equals " + str(players))
+              print("number of players created equals " + str(players))
+              #assign a name to each player
+
+
+              player_array= []
+
+              for i in range(0, players):
+                  while True:
+                      print("i = " + str (i))
+                      name = input("What is the name of player " + str(i) + "?")
+                      if not name in player_array:
+                          player_array.append(name)
+                          break
+                      else:
+                          print("cant have duplicate names")
+                    #'return'(names)
+
+              # get out of while loop
+              break
+
 #names and players
     except ValueError:
         print ("must be a number")
+ #lists player # and player name
+ for count in range(len(player_array)):
+     print ("Player " + str(count) + " " + player_array[count])
+
+"""
 names = {}
 for i in range (1, players +1):
     while True:
@@ -43,12 +79,10 @@ if new_position in 'snake_squares':
 elif new_position in 'ladder_squares':
  print ("player has climbed up ladder and is now on square{}")
 else:
- 'return' (new_position)
-#animations
-font = font.render.SysFont("Comic Sans MS", 40)
-screen_title = (str(title), (RED))
-screen.blit(screen_title, [ 175, 300 ]
-clock.tick(50)/
-pygame.quit()
-#matthew is dumbo
-#test
+
+
+"""
+
+
+# execution
+setup_game()
