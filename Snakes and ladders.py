bbde5c70c65f54270ecb2951cb1ace42f4553064
kmanchu <leafhockey13@gmail.com>
2018-06-10 22:12:16 -0400

    create roll dice function

--- Snakes and ladders.py
+++ Snakes and ladders.py
@@ -1,3 +1,4 @@
+import random
 import logging
 logging.basicConfig(filename='snakes_ladders.log',level=logging.DEBUG)
 """
@@ -5,7 +6,6 @@ logging.basicConfig(filename='snakes_ladders.log',level=logging.DEBUG)
 -number of players
 -postion of players
 -Dice function
-
 
 """
 logging.info('Start of program')
@@ -23,66 +23,88 @@ def setup_game():
               print("number of players created equals " + str(players))
               #assign a name to each player
 
-
+              #list of all the players in the game that stores the names
               player_array= []
-
+              position_array= []
+              #0 is the start of the range. players is the end of a range
               for i in range(0, players):
                   while True:
                       print("i = " + str (i))
                       name = input("What is the name of player " + str(i) + "?")
+                      #checks to see if name has already be chosen. If it has been chosen, pick a new one.
                       if not name in player_array:
                           player_array.append(name)
+                          position_array.append(0)
+                          #When you enter a name that's not on the list. means you're good
                           break
                       else:
                           print("cant have duplicate names")
                     #'return'(names)
 
-              # get out of while loop
+
+              # When you choose players # between 2-4
               break
 
 #names and players
     except ValueError:
         print ("must be a number")
- #lists player # and player name
+
  for count in range(len(player_array)):
+
+     print ("im at count = " + str (count))
+     #lists player number and names
      print ("Player " + str(count) + " " + player_array[count])
+     print ("Player " + str(count) +" is at" + " " + str(position_array[count]))
 
 """
-names = {}
-for i in range (1, players +1):
-    while True:
-        name = input ("What is the name of player{}").format (i)
-        if not name in names:
-            names(name) = 0
-            break
-else:
-   print("cant have duplicate names")
-'return' (names)
+-fuction to roll dice
+-returns number between 1-6
+"""
+def roll_dice():
+    return random.randint(1,6)
 
-#screen display
-
-#screen caption
-
-title = 'snakes and ladders'
-
-#If you land on a snake square you would slide down the snake to bottom square
-#If you land on a ladder square you would climb up the ladder to top square
-def move_player( player,  current_position):
- snake_squares = {16: 5, 34: 29, 64: 38 , 77: 4,  93: 24}
- ladder_squares = {4: 19, 22: 48, 30: 72, 51: 81, 61: 88}
-#roll dice
-roll= ('roll_dice')
-new_position= ('current_position' + roll)
-print ("{} rolled a {1} and is now currently on {2}")
-if new_position in 'snake_squares':
- print ("player has slid down snake and is now on square {}")
-elif new_position in 'ladder_squares':
- print ("player has climbed up ladder and is now on square{}")
-else:
 
 
 """
+    names = {}
+    for i in range (1, players +1):
+        while True:
+            name = input ("What is the name of player{}").format (i)
+            if not name in names:
+                names(name) = 0
+                break
+    else:
+       print("cant have duplicate names")
+    'return' (names)
+    
+    #screen display
+    
+    #screen caption
+    
+    title = 'snakes and ladders'
+    
+    #If you land on a snake square you would slide down the snake to bottom square
+    #If you land on a ladder square you would climb up the ladder to top square
+    def move_player( player,  current_position):
+     snake_squares = {16: 5, 34: 29, 64: 38 , 77: 4,  93: 24}
+     ladder_squares = {4: 19, 22: 48, 30: 72, 51: 81, 61: 88}
+    #roll dice
+    roll= ('roll_dice')
+    new_position= ('current_position' + roll)
+    print ("{} rolled a {1} and is now currently on {2}")
+    if new_position in 'snake_squares':
+     print ("player has slid down snake and is now on square {}")
+    elif new_position in 'ladder_squares':
+     print ("player has climbed up ladder and is now on square{}")
+    else:
+
+"""
+
+
+
 
 
 # execution
 setup_game()
+
+print("player rolled " + str(roll_dice()))
