7322c9d728ad49aecd0e19bbda0c6fead4d1410a
kmanchu <leafhockey13@gmail.com>
2018-06-11 20:03:29 -0400

    update

--- Snakes and ladders.py
+++ Snakes and ladders.py
@@ -63,46 +63,12 @@ def setup_game():
 def roll_dice():
     return random.randint(1,6)
 
-
-
 """
-    names = {}
-    for i in range (1, players +1):
-        while True:
-            name = input ("What is the name of player{}").format (i)
-            if not name in names:
-                names(name) = 0
-                break
-    else:
-       print("cant have duplicate names")
-    'return' (names)
-    
-    #screen display
-    
-    #screen caption
-    
-    title = 'snakes and ladders'
-    
-    #If you land on a snake square you would slide down the snake to bottom square
-    #If you land on a ladder square you would climb up the ladder to top square
-    def move_player( player,  current_position):
-     snake_squares = {16: 5, 34: 29, 64: 38 , 77: 4,  93: 24}
-     ladder_squares = {4: 19, 22: 48, 30: 72, 51: 81, 61: 88}
-    #roll dice
-    roll= ('roll_dice')
-    new_position= ('current_position' + roll)
-    print ("{} rolled a {1} and is now currently on {2}")
-    if new_position in 'snake_squares':
-     print ("player has slid down snake and is now on square {}")
-    elif new_position in 'ladder_squares':
-     print ("player has climbed up ladder and is now on square{}")
-    else:
-
+-move function
+-current position
+-next position
+-number that was rolled
 """
-
-
-
-
 
 # execution
 setup_game()
