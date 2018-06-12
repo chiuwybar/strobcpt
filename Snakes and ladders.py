21cbedaae7c0c75de635b2a019bed655fec152f7
kmanchu <leafhockey13@gmail.com>
2018-06-11 22:37:11 -0400

    added message box and added player class

--- Snakes and ladders.py
+++ Snakes and ladders.py
@@ -1,6 +1,29 @@
+from tkinter import *
+import tkinter.simpledialog
+
+#import tkMessageBox
 import random
 import logging
 logging.basicConfig(filename='snakes_ladders.log',level=logging.DEBUG)
+
+
+root = Tk()
+#w = Label(root, text="My Program")
+#name = tkinter
+#name= tkinter.simpledialog.askstring("Name of player", "What is your name")
+#print ("your name is " + name)
+class Player:
+    def __init__(self,name):
+        self.name = name
+        self.current_position = 0
+    def __str__(self):
+       return "player " + self.name
+
+    def get_current_position(self):
+        return self.current_position
+
+    def set_current_position(self,steps):
+        self.current_position = self.current_position + steps
 """
 -setup game requires
 -number of players
@@ -8,33 +31,39 @@ logging.basicConfig(filename='snakes_ladders.log',level=logging.DEBUG)
 -Dice function
 
 """
+
+#numbweof=tkinter.simpledialog.askstring("number of players", "How many players are there?")
+#print ("there are " + str(numbweof))
 logging.info('Start of program')
 def setup_game():
- players= 2
+ #players= 2
  while True:
     try:
-          print("how many players are in the game?")
-          players= int(input())
-          if players > 4 or players < 2:
+
+          #print("how many players are in the game?")
+          #num_players= int(input("how many players are in the game?"))
+          num_players= int(tkinter.simpledialog.askinteger("number of players", "How many players are there?"))
+          if num_players > 4 or num_players < 2:
               print("must be lower than 5 or higher than 1")
           else:
               #the right number of players had been defined...
-              logging.info("number of players created equals " + str(players))
-              print("number of players created equals " + str(players))
+              logging.info("number of players created equals " + str(num_players))
+              print("number of players created equals " + str(num_players))
               #assign a name to each player
 
               #list of all the players in the game that stores the names
-              player_array= []
+              players= []
               position_array= []
               #0 is the start of the range. players is the end of a range
-              for i in range(0, players):
+              for i in range(0, num_players):
                   while True:
                       print("i = " + str (i))
-                      name = input("What is the name of player " + str(i) + "?")
+                     # name = input("What is the name of player " + str(i) + "?")
+                      name = tkinter.simpledialog.askstring("Name of player" + str(i), "What is your name")
                       #checks to see if name has already be chosen. If it has been chosen, pick a new one.
-                      if not name in player_array:
-                          player_array.append(name)
-                          position_array.append(0)
+                      if not name in players:
+                          players.append(Player(name))
+                         # position_array.append(0)
                           #When you enter a name that's not on the list. means you're good
                           break
                       else:
@@ -49,12 +78,14 @@ def setup_game():
     except ValueError:
         print ("must be a number")
 
- for count in range(len(player_array)):
+#test to make sure number of players is correct
+ for count in range(len(players)):
 
-     print ("im at count = " + str (count))
+     #print ("im at count = " + str (count))
      #lists player number and names
-     print ("Player " + str(count) + " " + player_array[count])
-     print ("Player " + str(count) +" is at" + " " + str(position_array[count]))
+     print ("Player " + str(count) + " " + str(players[count]))
+    # print ("Player " + str(count) +" is at" + " " + str(position_array[count]))
+     print("Player " + str(count) + " " + str(players[count])+" is at" + " " + str(players[count].get_current_position()))
 
 """
 -fuction to roll dice
@@ -69,8 +100,9 @@ def roll_dice():
 -next position
 -number that was rolled
 """
-
 # execution
 setup_game()
 
-print("player rolled " + str(roll_dice()))
+#print("player rolled " + str(roll_dice()))
+
+
